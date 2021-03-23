cc123b5acd16bddb4b8d12905926f065ba216166
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-23 16:32:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.a17e6b93b9bdd09678f58e991cfa36ab.js"
 );
 
 self.addEventListener('message', (event) => {
